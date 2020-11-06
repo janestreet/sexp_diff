921df0f5e8d7fa731fc9ade785ca194a991fad06
Xavier Clerc <xclerc@janestreet.com>
2020-11-06 12:00:13 +0000

    v0.15~preview.124.03+65

---
+++ src/sexp_diff.ml
@@ -0,0 +1,6 @@
+module Algo = Algo
+module Diff = Diff
+module Display = Display
+
+(** Expose display helpers for use by the js-friendly library "sexp_diff_js" *)
+module Display_util_internal = Display_util_internal
